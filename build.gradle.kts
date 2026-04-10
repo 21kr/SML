a04bd1b4480438f5553909565e473382f9f001e2
21kr <21krpaligutan@asist.edu.ph>
2026-04-10 10:11:10 +0800

    Configure Hilt in app and initialize Application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.4.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
